cd3d42c6bbe142c018a37fb046a5bd0b15f5f1e3
systemnb <systemnb@163.com>
2025-07-20 22:37:03 +0800

    Update memory.h

--- code/memory.h
+++ code/memory.h
@@ -1,6 +1,8 @@
 #include <linux/kernel.h>
 #include <linux/sched.h>
 
+static inline bool valid_phys_addr_range(phys_addr_t addr, size_t count);
+
 phys_addr_t translate_linear_address(struct mm_struct* mm, uintptr_t va);
 
 bool read_physical_address(phys_addr_t pa, void* buffer, size_t size);
